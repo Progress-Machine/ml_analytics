c8038f75d4df2e42242e51d06d533613b0c54395
Lareton <79097392+Lareton@users.noreply.github.com>
2023-08-13 13:55:20 +0700

    add full pipeline code

---
+++ full_pipeline.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import requests
+
+
+from wb_parser import parse_link
+from get_regressor_predict_by_product import get_predict
+from get_analytics import get_analytics
+
+def get_predict_by_link(link):
+    """получает ссылку - парсит данные, делает аналитику и получает предикт выручки"""
+    dct_product_data = parse_link(link)
+    img_link = dct_product_data["img_link"]
+
+    img_data = requests.get(img_link).content
+    with open('cur_img.jpg', 'wb') as handler:
+        handler.write(img_data)
+    img = Image.open("cur_img.jpg")
+
+    revenue_predict = get_predict(dct_product_data, img)      # предикт выручки
+
+    dct_product_data["price"] = float(dct_product_data["price"])
+    dct_product_data["order_count"] = float(dct_product_data["order_count"])
+    analys_data = get_analytics(dct_product_data)
+
+    return dct_product_data, revenue_predict, analys_data
+
+
+def get_predict_for_new_product(dct_product_data):
+    """для измененного товара делает аналитику и получает предикт выручки"""
+    img_link = dct_product_data["img_link"]
+
+    img_data = requests.get(img_link).content
+    with open('cur_img.jpg', 'wb') as handler:
+        handler.write(img_data)
+    img = Image.open("cur_img.jpg")
+
+    revenue_predict = get_predict(dct_product_data, img)  # предикт выручки
+    analys_data = get_analytics(dct_product_data)
+
+    return dct_product_data, revenue_predict, analys_data
